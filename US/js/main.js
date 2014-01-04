a4aa6dc59f06d8d3b91712886dccba6452ad5a70
Florian Oswald <florian.oswald@gmail.com>
2014-01-04 10:23:55 +0000

    max fixed the graph scale by moving scaleSteps parameter. would be good to format y-axis with thousand delimiters. the number 270000000 is hard to read.

--- US/js/main.js
+++ US/js/main.js
@@ -4,7 +4,7 @@ $(function() {
 	function nf(num,dig,dec,sep) {
 		var x = [];
 		var s = (num<0?"-":"");
-			num = Math.abs(num).toFixed(dig).split(",");
+			num = Math.abs(num).toFixed(dig).split(".");
 		var r = num[0].split("").reverse();
 		for (var i=1;i<=r.length;i++) {
 			x.unshift(r[i-1]);
@@ -36,15 +36,15 @@ $(function() {
 
 	// detect touch & disable slider
 	$("#slider-dist input").bind("slider:ready slider:changed", function (event, data) {
-		$(this).nextAll(".slider-value").html(nf(data.value, 0, ",", "."));
+		$(this).nextAll(".slider-value").html(nf(data.value, 0, ".", ","));//Max: komma und punkt vertauscht!
 	});
 
 	$("#slider-taxfree input").bind("slider:changed", function (event, data) {
-		$(this).nextAll(".slider-value").html(nf(data.value, 0, ".", ",")+" $");
+		$(this).nextAll(".slider-value").html("$" + nf(data.value, 0, ".", ","));//Max: komma und punkt vertauscht, $!
 	});
 
 	$("#slider-taxamt input").bind("slider:changed", function (event, data) {
-		$(this).nextAll(".slider-value").html(nf(data.value, 1, ".", ",")+"% annually");
+		$(this).nextAll(".slider-value").html(nf(data.value, 1, ".", ",")+"% per year");//Max: komma und punkt vertauscht, per year!
 	});
 
 	// rechner1.html
@@ -88,16 +88,9 @@ $(function() {
 		drawMyTaxModel();
 	});
 
-	// US wealth distribution data.
-	// SCF data
-	// check ../../p99-wealthometer.xls
-	// TODO this is the wrong data!
-	// this is the german wealth distribution
-	// needs to be changed.
-	// DONE (Max)
-at = [ -31888 , -17879 , -11099 , -7729 , -5461 , -3794 , -2753 , -1690 , -1109 , -564 , -176 , 0 , 0 , 32 , 203 , 430 , 655 , 849 , 1063 , 1346 , 1547 , 1784 , 2073 , 2329 , 2600 , 2949 , 3225 , 3564 , 3917 , 4355 , 4850 , 5551 , 6103 , 6554 , 7046 , 7645 , 8258 , 8913 , 9893 , 10765 , 11750 , 12545 , 13743 , 14966 , 16013 , 17378 , 18453 , 20209 , 21532 , 22832 , 24319 , 25887 , 27384 , 28895 , 30387 , 32530 , 34787 , 36695 , 38693 , 41459 , 44809 , 47902 , 51632 , 54793 , 58220 , 62330 , 66243 , 69602 , 73245 , 78469 , 83657 , 89478 , 95814 , 102371 , 110170 , 116615 , 124613 , 132943 , 141056 , 151244 , 161780 , 173156 , 189176 , 206512 , 221557 , 244762 , 268966 , 293184 , 320617 , 361415 , 411628 , 461311 , 526219 , 606727 , 741520 , 917033 , 1161447 , 1633264 , 2734475];	
-
-
+	// static data
+	at = [ -31888 , -17879 , -11099 , -7729 , -5461 , -3794 , -2753 , -1690 , -1109 , -564 , -176 , 0 , 0 , 32 , 203 , 430 , 655 , 849 , 1063 , 1346 , 1547 , 1784 , 2073 , 2329 , 2600 , 2949 , 3225 , 3564 , 3917 , 4355 , 4850 , 5551 , 6103 , 6554 , 7046 , 7645 , 8258 , 8913 , 9893 , 10765 , 11750 , 12545 , 13743 , 14966 , 16013 , 17378 , 18453 , 20209 , 21532 , 22832 , 24319 , 25887 , 27384 , 28895 , 30387 , 32530 , 34787 , 36695 , 38693 , 41459 , 44809 , 47902 , 51632 , 54793 , 58220 , 62330 , 66243 , 69602 , 73245 , 78469 , 83657 , 89478 , 95814 , 102371 , 110170 , 116615 , 124613 , 132943 , 141056 , 151244 , 161780 , 173156 , 189176 , 206512 , 221557 , 244762 , 268966 , 293184 , 320617 , 361415 , 411628 , 461311 , 526219 , 606727 , 741520 , 917033 , 1161447 , 1633264 , 2734475];	
+	// at.push(Number.MAX_VALUE);
 
 	// lables
 	labels = new Array(100);
@@ -135,9 +128,9 @@ at = [ -31888 , -17879 , -11099 , -7729 , -5461 , -3794 , -2753 , -1690 , -1109 
 		$("#f-actual").html(myPosition);
 		$("#f-estimate").html(estimate);
 		$(".pos-estimate").html(estimate);
-		$(".val-estimate").html(nf(at[estimate-1], 0, ",", "."));
+		$(".val-estimate").html(nf(at[estimate-1], 0, ".", ","));//Max: komma und punkt vertauscht!
 
-		$(".val-actual").html(nf(balance, 0, ",", "."));
+		$(".val-actual").html(nf(balance, 0, ".", ","));//Max: komma und punkt vertauscht!
 		$(".pos-actual").html(myPosition);
 
 		// set up graph
@@ -158,7 +151,7 @@ at = [ -31888 , -17879 , -11099 , -7729 , -5461 , -3794 , -2753 , -1690 , -1109 
 		var options = {
 			// animation : false,
 			scaleOverride : true,
-			scaleSteps : 17,
+			scaleSteps : 28, //comment Max: das bestimmt die chart hoehe!
 			scaleStepWidth : 100000,
 			scaleStartValue : -100000,
 			scaleFontFamily : "'Lato'",
@@ -195,23 +188,20 @@ at = [ -31888 , -17879 , -11099 , -7729 , -5461 , -3794 , -2753 , -1690 , -1109 
 			accumTax += proptax[i];
 		}
 
-		percapita = 0.85 * accumTax / 100.0;
+		percapita = 0.80 * accumTax / 100.0;
 
 		myModel = [];
 		for (i = at.length - 1; i >= 0; i--) {
 			myModel[i] = percapita - proptax[i];
 		}
 
-		$("#percapita").html(nf(percapita,2,".",",") + " $");
+		$("#percapita").html("$" + nf(percapita,2,".",","));//Max: komma und punkt vertauscht, $!
 		mytax = 1.0 * Math.max(0, balance - taxfree) * taxamt / 100.0;
-		$("#mytax").html(nf(mytax, 2, ".", ",") + " $");
-		$("#mytaxbalance").html(nf(percapita - mytax, 2, ".", ",") + " $");
+		$("#mytax").html("$" + nf(mytax, 2, ".", ","));//Max: komma und punkt vertauscht, $!
+		$("#mytaxbalance").html("$" + nf(percapita - mytax, 2, ".", ","));//Max: komma und punkt vertauscht, $!
 
-		//min = Math.floor(myModel[98]/1000)*1000;	this is poor. hardcoded the top value to the 98-th element of a data vector (which is not checked on length). :-(
-		//min = Math.floor(myModel.length/1000)*1000;
-		//max = Math.ceil(myModel[0]/1000)*1000;		also: MIN AND MAX ARE REVERSED?!
-		max = Math.floor((myModel.length)/1000)*1000;
-		min = Math.ceil(myModel[0]/1000)*1000;
+		min = Math.floor(myModel[98]/1000)*1000;
+		max = Math.ceil(myModel[0]/1000)*1000;
 		steps = (max - min ) / 1000;
 
 		// set up graph
@@ -232,7 +222,6 @@ at = [ -31888 , -17879 , -11099 , -7729 , -5461 , -3794 , -2753 , -1690 , -1109 
 			scaleSteps : steps,
 			scaleStepWidth : 1000,
 			scaleStartValue : min,
-			scaleEndValue : max,
 			scaleFontFamily : "'Lato'",
 			barShowStroke : false,
 			barStrokeWidth : 0,
