2fd35a0708306594403ef0c66512592700ee1afa
Florian Oswald <florian.oswald@gmail.com>
2014-02-06 11:14:00 +0000

    italian version updated

---
+++ IT/js/main.js
@@ -0,0 +1,233 @@
+$(function() {
+	// zahl, nachkomma, dezimal, tausender
+	// zahlenformatierung
+	function nf(num,dig,dec,sep) {
+		var x = [];
+		var s = (num<0?"-":"");
+			num = Math.abs(num).toFixed(dig).split(".");
+		var r = num[0].split("").reverse();
+		for (var i=1;i<=r.length;i++) {
+			x.unshift(r[i-1]);
+			if ( i%3===0 && i != r.length )
+				x.unshift(sep);
+		}
+		return s+x.join("")+(num[1]?dec+num[1]:"");
+	}
+
+	var ie = (function(){
+		var undef,rv = -1; // Return value assumes failure.
+		if (navigator.appName == 'Microsoft Internet Explorer')
+		{
+			var ua = navigator.userAgent;
+			var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+			if (re.exec(ua) != null)
+				rv = parseFloat( RegExp.$1 );
+		}
+		return ((rv > -1) ? rv : undef);
+	}());
+
+	console.log(ie);
+	if ( ie ) {
+		if ( $('#results-display').length )
+			$('#results-display')[0].style.setProperty( 'height', '600px', 'important' );
+	}
+
+
+
+	// detect touch & disable slider
+	$("#slider-dist input").bind("slider:ready slider:changed", function (event, data) {
+		$(this).nextAll(".slider-value").html(nf(data.value, 0, ",", "."));
+	});
+
+	$("#slider-taxfree input").bind("slider:changed", function (event, data) {
+		$(this).nextAll(".slider-value").html(nf(data.value, 0, ",", ".")+" €");
+	});
+
+	$("#slider-taxamt input").bind("slider:changed", function (event, data) {
+		$(this).nextAll(".slider-value").html(nf(data.value, 1, ",", ".")+"% per annum");
+	});
+
+	// rechner1.html
+	$("body").on("click", "#button-dist", function() {
+		var value = $("#slider-dist input").val();
+		$.cookie("estimate", value);
+	});
+
+	// rechner2.html
+	
+	if ( $(".rechner2").length ) {
+		// read cookies
+		$("#sachvermoegen").val(parseFloat($.cookie("sachvermoegen")) || "");
+		$("#finanzvermoegen").val(parseFloat($.cookie("finanzvermoegen")) || "");
+		$("#schulden").val(parseFloat($.cookie("schulden")) || "");
+		$("#mitglieder").val(parseFloat($.cookie("mitglieder")) || "");
+
+		// write cookies on click, draw chart
+		$("body").on("click", "#button-calculate", function() {
+			$.cookie("sachvermoegen", $("#sachvermoegen").val().replace(/\D/g, '' ));
+			$.cookie("finanzvermoegen", $("#finanzvermoegen").val().replace(/\D/g, '' ));
+			$.cookie("schulden", $("#schulden").val().replace(/\D/g, '' ));
+			$.cookie("mitglieder", $("#mitglieder").val().replace(/\D/g, '' ) || 1);
+
+		// read cookies - sanitized :)
+		$("#sachvermoegen").val(parseFloat($.cookie("sachvermoegen")) || "");
+		$("#finanzvermoegen").val(parseFloat($.cookie("finanzvermoegen")) || "");
+		$("#schulden").val(parseFloat($.cookie("schulden")) || "");
+		$("#mitglieder").val(parseFloat($.cookie("mitglieder")) || "");
+
+			drawWhoHasWhat();
+		});
+	}
+
+
+
+	// rechner3.html
+	$("body").on("click", "#button-simulate", function() {
+		$.cookie("steuerfrei", $("#slider-taxfree input").val());
+		$.cookie("steuersatz", parseFloat($("#slider-taxamt input").val()).toFixed(1));
+		drawMyTaxModel();
+	});
+
+	// IT wealth distribution data.
+	// check ../../data/p99-wealthometer.xls
+	at =  [-1000,0,67,265,500,717,1000,1300,1500,1875,2344,2678,3150,3500,4077,4786,5400,6000,7014,7915,8767,9983,11040,12500,13974,15250,16971,18672,20739,22882,25000,27377,29767,31750,33714,35250,37038,39481,41073,43018,44616,46664,48419,50489,51667,53500,55556,57302,59102,60730,62196,63434,65500,67629,69625,72088,73969,75433,77841,80479,83157,85000,88090,90125,93416,95511,98910,101250,104537,107244,110167,113024,117500,121618,125700,130036,134750,140503,145000,150250,156300,162453,169000,176000,183467,193000,203229,212780,217750,233932,249000,263175,283333,310750,340724,389167,479308,600864,873196];
+
+	// lables
+	labels = new Array(100);
+	for (var i=1; i<labels.length; i++) {
+		if ( (i) % 10 === 0) {
+			labels[i] = (i)+" %";
+		} else {
+			labels[i] = "";
+		}
+	}
+	labels[0] = "";
+	labels[1] = "1%";
+	labels.pop();
+	labels.push("99%");
+
+	findMyPosition = function(balance) {
+		for (i = 0; i < at.length; i++) {
+			if ( at[i] > balance ) {
+				break;
+			}
+		}
+		return i;
+	};
+
+	drawWhoHasWhat = function( ) {
+		// calculate variables
+		var sachvermoegen = parseFloat($.cookie("sachvermoegen")) || 0;
+		var finanzvermoegen = parseFloat($.cookie("finanzvermoegen")) || 0;
+		var schulden = parseFloat($.cookie("schulden")) || 0;
+		var mitglieder = parseFloat($.cookie("mitglieder")) || 1;
+
+		balance = (sachvermoegen + finanzvermoegen - schulden) / mitglieder;
+		estimate = parseFloat($.cookie("estimate")) || 0;
+		myPosition = findMyPosition(balance);
+		$("#f-actual").html(myPosition);
+		$("#f-estimate").html(estimate);
+		$(".pos-estimate").html(estimate);
+		$(".val-estimate").html(nf(at[estimate-1], 0, ",", "."));
+
+		$(".val-actual").html(nf(balance, 0, ",", "."));
+		$(".pos-actual").html(myPosition);
+
+		// set up graph
+		var ctx = $("#results-display").get(0).getContext("2d");
+		var data = {
+			labels : labels,
+			datasets : [{
+				fillColor : "#BE3C14",
+				strokeColor : "white",
+				highLightPos: myPosition-1,
+				highLightColor : "#73A5B1",
+				HLP2: estimate-1,
+				HLC2: "green",
+				data : at
+			}]
+		};
+
+		var options = {
+			// animation : false,
+			scaleOverride : true,
+			scaleSteps : 10,
+			scaleStepWidth : 100000,
+			scaleStartValue : -100000,
+			scaleFontFamily : "'Lato'",
+			barShowStroke : false,
+			barStrokeWidth : 0,
+			barValueSpacing : 1,
+			barDatasetSpacing : 0
+		};
+		var myNewChart = new Chart(ctx).Bar(data, options);
+		$(".showlater").slideDown();
+	};
+
+
+	drawMyTaxModel = function() {
+		sachvermoegen = parseFloat($.cookie("sachvermoegen")) || 0;
+		finanzvermoegen = parseFloat($.cookie("finanzvermoegen")) || 0;
+		schulden = parseFloat($.cookie("schulden")) || 0;
+		mitglieder = $.cookie("mitglieder");
+		balance = (sachvermoegen + finanzvermoegen - schulden) / mitglieder;
+		if (isNaN(balance))
+			balance = 0.0;
+		estimate = parseFloat($.cookie("estimate"));
+		taxfree = parseFloat($.cookie("steuerfrei"));
+		taxamt = parseFloat($.cookie("steuersatz"));
+
+		myPosition = findMyPosition(balance);
+
+		// calculate (absolute) tax for every class
+		accumTax = 0.0;
+		proptax = [];
+		var i;
+		for (i = at.length - 1; i >= 0; i--) {
+			proptax[i] = 1.0 * Math.max(0, at[i] - taxfree) * taxamt / 100.0;
+			accumTax += proptax[i];
+		}
+
+		percapita = 0.85 * accumTax / 100.0;
+
+		myModel = [];
+		for (i = at.length - 1; i >= 0; i--) {
+			myModel[i] = percapita - proptax[i];
+		}
+
+		$("#percapita").html(nf(percapita,2,",",".") + " €");
+		mytax = 1.0 * Math.max(0, balance - taxfree) * taxamt / 100.0;
+		$("#mytax").html(nf(mytax, 2, ",", ".") + " €");
+		$("#mytaxbalance").html(nf(percapita - mytax, 2, ",", ".") + " €");
+
+		min = Math.floor(myModel[98]/1000)*1000;
+		max = Math.ceil(myModel[0]/1000)*1000;
+		steps = (max - min ) / 1000;
+
+		// set up graph
+		var ctx = $("#results-display").get(0).getContext("2d");
+		var data = {
+			labels : labels,
+			datasets : [{
+				fillColor : "#BE3C14",
+				strokeColor : "white",
+				highLightPos: myPosition-1,
+				highLightColor : "#73A5B1",
+				data : myModel
+			}]
+		};
+
+		var options = {
+			scaleOverride : true,
+			scaleSteps : steps,
+			scaleStepWidth : 1000,
+			scaleStartValue : min,
+			scaleFontFamily : "'Lato'",
+			barShowStroke : false,
+			barStrokeWidth : 0,
+			barValueSpacing : 1,
+			barDatasetSpacing : 0
+		};
+		var myNewChart = new Chart(ctx).Bar(data, options);
+	};
+});
